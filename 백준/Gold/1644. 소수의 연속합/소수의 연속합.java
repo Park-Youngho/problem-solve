f967fc3ff5117fe25b5c0e6ef0433a1fc704891b
Park-Youngho <ypark6529@gmail.com>
2025-01-28 17:19:33 +0900

    Update 소수의 연속합.java

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -6,34 +6,36 @@ public class Main {
     static int ret =0;
     static int[] sumarr;
     static int[] arr;
-    static boolean check(int n){
-        if(n==1) return false;
-        if(n==2) return true;
+    static boolean[] isPrime;
+    static List<Integer> v = new ArrayList<>();
+    static void check(int n){
+
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
         for(int i=2; i<=Math.sqrt(n); i++){
-            if((n % i) == 0) return false;
+            for(int j=i*i; j<=n; j+=i){
+                isPrime[j] = false;
+            }
         }
-        return true;
+        for(int i=0; i<isPrime.length-1; i++){
+            if(isPrime[i]) v.add(i);
+        }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        isPrime = new boolean[n+1];
+        check(n);
 
-        arr = new int[n+1];
-        int idx =0;
-        for(int i=2; i<n; i++){
-            if(check(i)){
-                arr[idx] = i;
-                idx++;
-            }
-        }
-        sumarr= new int[idx+1];
-        for(int i=1; i<=idx; i++){
-            sumarr[i] += (sumarr[i-1] + arr[i-1]);
+        sumarr= new int[v.size()+1];
+
+        for(int i=1; i<=v.size(); i++){
+            sumarr[i] += (sumarr[i-1] + v.get(i-1));
         }
 
         int l = 0;
         int r = 1;
-        while(r < idx+1){
+        while(r <= v.size()){
             int sum =sumarr[r]-sumarr[l];
             if(sum < n) r++;
             else if(sum > n) l++;
@@ -42,7 +44,9 @@ public class Main {
                 r++;
             }
         }
-        if(check(n)) ret++;
+        if (isPrime[n]) {
+            ret++;
+        }
         System.out.println(ret);
     }
 
